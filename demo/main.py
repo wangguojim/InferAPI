e3e987353921eb6df96e6a5724c5dfe3c7279ce5
wnagguojim <862876363@qq.com>
2022-06-24 06:46:29 +0000

    bert inference api

--- demo/main.py
+++ demo/main.py
@@ -1,23 +1,14 @@
 from fastapi import FastAPI
 import transformers
-
+from transformers import pipeline
 app = FastAPI()
 
-from transformers import BertTokenizer
-tokenizer = BertTokenizer(vocab_file="/data/wang/models/bmm/vocab/bpe_cn/vocab.txt")
+unmasker = pipeline('fill-mask', model='bert-base-uncased')
 
-
-
-
+#
 @app.get("/{sentence}")
 async def root(sentence):
-
-    tokens = tokenizer.tokenize(sentence)
-    token_id = tokenizer.convert_tokens_to_ids(tokens)
-    id2token = tokenizer.convert_ids_to_tokens(token_id)
-
-    return {"tokens": tokens,
-            "token_id":token_id,
-            "id2token":id2token,
+    output=unmasker("Hello I'm a [MASK] model.")
+    return {"output": output,
             'code': '200'
             }
